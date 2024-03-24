8909aa3617f2558d00fb7be755441dafa7d422b7
Razieh Rahimi <raz.rhm.87@gmail.com>
2024-03-24 15:47:26 +0330

    update index.htm and show wather details

--- example/loockup/views.py
+++ example/loockup/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+import json
+import requests
+
+result = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=51838DC2-B14D-4FA5-A746-6954DE53931B")
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html",{})
+
+    try:
+        api = json.loads(result.content)
+    except Exception as e:
+        api = 'Error'
+    return render(request,"index.html",{'api' : api})
+# https://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=20002&distance=25&API_KEY=51838DC2-B14D-4FA5-A746-6954DE53931B
+
 
 def about(request):
     return render(request,"about.html",{})
