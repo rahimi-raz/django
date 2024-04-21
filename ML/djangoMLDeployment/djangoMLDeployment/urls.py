0c0d5819d9e3a793c509b7d1b5f97b166e25ed06
Razieh Rahimi <raz.rhm.87@gmail.com>
2024-04-21 20:03:52 +0330

    add ML model

--- ML/djangoMLDeployment/djangoMLDeployment/urls.py
+++ ML/djangoMLDeployment/djangoMLDeployment/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('basicConsepts.urls')),
+    # path('',include('basicConsepts.urls')),
+    path('',include('mlApp.urls')),
     path('admin/', admin.site.urls),
 ]
